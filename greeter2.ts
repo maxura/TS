677d80fbe9544126f36bd8f15fb758c7d5c57090
Maxim Musienko <mmusienko@codenvy.com>
2018-01-24 00:23:49 +0200

    add new samples

---
+++ greeter2.ts
@@ -0,0 +1,7 @@
+function greeter2(person:string) {
+    return "Hello, " + person;
+}
+
+let user2 = "Jane User";
+
+document.body.innerHTML = greeter(user2);
---
+++ greeter3.ts
@@ -0,0 +1,7 @@
+function greeter3(person:string) {
+    return "Hello, " + person;
+}
+
+let user3 = [1,2,3];
+
+document.body.innerHTML = greeter3(user3);
