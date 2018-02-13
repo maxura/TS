b574a66a55153b28bcb0477539809f690c8db84c
Maxim Musienko <mmusienko@codenvy.com>
2018-02-13 18:06:09 +0200

    change structure on diff folders

---
+++ Javascript/DataStructure/Lists.js
@@ -0,0 +1,20 @@
+var	list	=	{
+    value:	1,
+    rest:	{
+        value:	2,
+        rest:	{
+            value:	3,
+            rest:	null
+        }
+    }
+};
+
+
+function tt() {
+    for (var i=0; i<3;i++){
+
+    }
+}
+
+console.log({value:0,	rest:	list});
+console.log({value:1,	rest:	list});
