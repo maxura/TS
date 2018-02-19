dabe8fc07f9524377505b1ee83bb3fdd6c5b59b5
Maxim Musienko <mmusienko@codenvy.com>
2018-02-19 20:23:01 +0200

    add trainer

---
+++ homeApp/randImages.js
@@ -0,0 +1,26 @@
+var elem = ["shorts.jpeg", "dress.jpeg", "cat.png"];
+
+
+function getImg() {
+    var elemSize=elem.length;
+    var randIndex = 0;
+    if (elemSize == 1)
+    {
+    return elem[0];
+    }
+
+    randIndex = Math.floor(Math.random() * (elemSize));
+    return elem[randIndex]
+
+}
+
+function addmg() {
+    var gimg = getImg();
+    var img = document.createElement("IMG");
+    img.src= "imgs/"+gimg;
+    img.setAttribute("style", "display: block;  border: 1px solid black; margin:15% auto;");
+    img.setAttribute("id", "1000")
+    document.body.appendChild(img);
+    var i = elem.indexOf(gimg);
+    elem.splice(i,1);
+}
