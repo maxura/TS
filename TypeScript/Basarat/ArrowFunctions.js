a6ed09acd0ecfbbb794eeb7a6299826fc9f18ee4
Maxim Musienko <mmusienko@codenvy.com>
2018-03-02 17:29:13 +0200

    add to Basarat book

---
+++ TypeScript/Basarat/ArrowFunctions.js
@@ -0,0 +1,23 @@
+//var inc = (x)=>x+1;
+// function Person(age) {
+//     this.age = age;
+//     this.growOld = function() {
+//         this.age++;
+//     }
+// }
+// var person = new Person(1);
+//
+// setTimeout(person.growOld,1000);
+//
+// setTimeout(function() { console.log(person.age); },2000);
+
+function Person(age) {
+    this.age = age;
+    this.growOld = () => {
+        this.age++;
+    }
+}
+var person = new Person(1);
+setTimeout(person.growOld,1000);
+
+setTimeout(function() { console.log(person.age); },2000);
---
+++ TypeScript/Basarat/ClassesEmit.js
@@ -0,0 +1,6 @@
+function Foo() {
+    this.bar =123;
+}
+
+var newFoo = new Foo();
+console.log(newFoo.bar)
