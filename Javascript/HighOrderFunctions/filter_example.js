360ff299b4c5a7c673dd730242dd952fb926dd60
Maxim Musienko <mmusienko@codenvy.com>
2018-04-22 23:58:42 +0300

    add smp

---
+++ Javascript/HighOrderFunctions/filter_example.js
@@ -0,0 +1,9 @@
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+ function condition(array) {
+     var item;
+     if (array.length>6){
+         item=array;
+     }
+         return item;
+ }
+console.log(words.filter(condition));
